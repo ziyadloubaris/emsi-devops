54d81a557c99655813dec95f5c297c4d96a6f688
ziyadloubaris <ziyadloubaris@gmail.com>
2021-05-07 02:32:47 +0100

    1-10

--- 0x06-api_advanced/1-top_ten.py
+++ 0x06-api_advanced/1-top_ten.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
-import sys
+"""
+Query Reddit API for titles of top ten posts of a given subreddit
+"""
+import requests
 
-if __name__ == '__main__':
-    top_ten = __import__('1-top_ten').top_ten
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+
+def top_ten(subreddit):
+    """
+        return top ten titles for a given subreddit
+        return None if invalid subreddit given
+    """
+    # get user agent
+    # https://stackoverflow.com/questions/10606133/ -->
+    # sending-user-agent-using-requests-library-in-python
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'My User Agent 1.0'})
+
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    r = requests.get(url, headers=headers).json()
+    top_ten = r.get('data', {}).get('children', [])
+    if not top_ten:
+        print(None)
+    for t in top_ten:
+        print(t.get('data').get('title'))
